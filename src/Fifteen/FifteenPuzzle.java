b52ddd37e6519e7672019b18873cffc8b03dbd2b
Christer Bengtsson <mail@christerbengtsson.se>
2024-10-28 08:19:46 +0100

    Initial commit

---
+++ src/Fifteen/FifteenPuzzle.java
@@ -0,0 +1,10 @@
+package Fifteen;
+
+
+// A classic 15-puzzle game implementation.
+
+public class FifteenPuzzle extends JFrame {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
